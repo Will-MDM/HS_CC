a038e584ba8a31d9e19e58f006a2e3ad3359b2ea
Will-MDM <92865795+Will-MDM@users.noreply.github.com>
2021-10-20 11:41:33 -0400

    Add files via upload

---
+++ getCompanyFromTicket.js
@@ -0,0 +1,39 @@
+const hubspot = require('@hubspot/api-client');
+
+exports.main = (event, callback) => {
+  const hubspotClient = new hubspot.Client({
+    apiKey: process.env.HAPIKEY
+  });
+ 
+  const thisObjectId = event.object.objectId;
+  
+   hubspotClient.crm.tickets.associationsApi.getAll(thisObjectId, "company")
+    .then(results => results.body.results[0].id)
+     .then(linkedCompanyId => {
+		hubspotClient.crm.companies.basicApi.getById(linkedCompanyId, ["address", "address2", "zip", "city", "phone", "mobile", "mobile_phone", "name"])
+         .then(results => {
+          let address = results.body.properties.address + (results.body.properties.address2 ? " " + results.body.properties.address2 : "") + " " + results.body.properties.zip + " " + results.body.properties.city
+          let phone = results.body.properties.phone|| results.body.properties.mobile_phone;
+          let name = results.body.properties.name;
+          
+          console.log(
+            "======== infos to return",
+            address, phone, name, results.body.properties.hs_object_id
+          )
+          callback({
+            outputFields: {
+              companyAddress: address,
+              companyPhone: phone, 
+              companyName: name,
+              companyId: results.body.properties.hs_object_id
+            }
+          });
+         })
+         .catch(err => {
+            console.log(err);
+         })
+    })
+  .catch(err => {
+    console.error(err)
+  });
+}
